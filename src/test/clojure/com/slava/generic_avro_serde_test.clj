10a6be95b70c64fa7f64889a39f27285554463ba
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2019-12-14 18:06:30 +0100

    Better tests

--- src/test/clojure/com/slava/generic_avro_serde_test.clj
+++ src/test/clojure/com/slava/generic_avro_serde_test.clj
@@ -5,10 +5,16 @@
            (io.confluent.kafka.schemaregistry.client MockSchemaRegistryClient)
            (io.confluent.kafka.streams.serdes.avro GenericAvroSerde)
            (com.slava NativeGenericAvroSerde)
-           (org.apache.avro.generic GenericRecordBuilder)
+           (org.apache.avro.generic GenericRecordBuilder GenericData$Record)
            (org.apache.kafka.common.errors SerializationException)))
 
-(deftest serialization
+(defn generic-data-record->map
+  [^GenericData$Record record]
+  (reduce (fn [m field] (assoc m (.name field) (.get record ^String (.name field))))
+          {}
+          (.getFields (.getSchema record))))
+
+(deftest field-logic
   (let [topic "simple-string"
         client (MockSchemaRegistryClient.)
         serde-config {"schema.registry.url" "mock://"}
@@ -24,17 +30,21 @@
                        .endRecord)]
         (.register client topic schema)
         (testing "empty record"
-          (let [m {}
+          (let [datum-map {}
+                actual-ser (->> (assoc datum-map "org.apache.avro.Schema" schema)
+                                (.serialize (.serializer actual-serde) topic)
+                                (.deserialize (.deserializer control-serde) topic))
+                actual-de (->> (.build (GenericRecordBuilder. ^Schema schema))
+                               (.serialize (.serializer control-serde) topic)
+                               (.deserialize (.deserializer actual-serde) topic))
                 control (->> (.build (GenericRecordBuilder. ^Schema schema))
                              (.serialize (.serializer control-serde) topic)
-                             (.deserialize (.deserializer control-serde) topic))
-                actual (->> (assoc m "org.apache.avro.Schema" schema)
-                            (.serialize (.serializer actual-serde) topic)
-                            (.deserialize (.deserializer control-serde) topic))]
-            (is (= control actual))))
+                             (.deserialize (.deserializer control-serde) topic))]
+            (is (= control actual-ser))
+            (is (= (generic-data-record->map actual-ser) actual-de))))
         (testing "populated record"
-          (let [m {"someField" "some value"}
-                actual (->> (assoc m "org.apache.avro.Schema" schema)
+          (let [datum-map {"someField" "some value"}
+                actual (->> (assoc datum-map "org.apache.avro.Schema" schema)
                             (.serialize (.serializer actual-serde) topic)
                             (.deserialize (.deserializer control-serde) topic))
                 control (->> (.build (GenericRecordBuilder. ^Schema schema))
@@ -43,118 +53,166 @@
             (is (= control actual))))))
     (testing "no-default field"
       (let [schema (-> (SchemaBuilder/builder)
-                       (.record "Simple")
+                       (.record "Record")
                        ^SchemaBuilder$RecordBuilder (.namespace "com.slava.core")
                        ^SchemaBuilder$FieldAssembler .fields
                        (.name "noDefault") .type .stringType .noDefault
                        .endRecord)]
         (.register client topic schema)
         (testing "missing field"
-          (try (let [m {}]
-                 (->> (assoc m "org.apache.avro.Schema" schema)
+          (try (let [datum-map {}]
+                 (->> (assoc datum-map "org.apache.avro.Schema" schema)
                       (.serialize (.serializer actual-serde) topic)))
                (throw (ex-message "test failed"))
                (catch AvroMissingFieldException e
                  (is (= "Field noDefault type:STRING pos:0 not set and has no default value" (.getMessage e))))))
         (testing "field present"
-          (let [m {"noDefault" ""}
-                actual (->> (assoc m "org.apache.avro.Schema" schema)
-                            (.serialize (.serializer actual-serde) topic)
-                            (.deserialize (.deserializer control-serde) topic))
-                control (->> (-> (GenericRecordBuilder. ^Schema schema) (.set "noDefault" "") .build)
+          (let [datum-map {"noDefault" ""}
+                datum-record (-> (GenericRecordBuilder. ^Schema schema)
+                                 (.set "noDefault" "")
+                                 .build)
+                actual-ser (->> (assoc datum-map "org.apache.avro.Schema" schema)
+                                (.serialize (.serializer actual-serde) topic)
+                                (.deserialize (.deserializer control-serde) topic))
+                actual-de (->> datum-record
+                               (.serialize (.serializer control-serde) topic)
+                               (.deserialize (.deserializer actual-serde) topic))
+                control (->> datum-record
                              (.serialize (.serializer control-serde) topic)
                              (.deserialize (.deserializer control-serde) topic))]
-            (is (= control actual))))
+            (is (= control actual-ser))
+            (is (= (generic-data-record->map actual-ser) actual-de))))
         (testing "bad type"
-          (try (let [m {"noDefault" 1}]
-                 (->> (assoc m "org.apache.avro.Schema" schema)
+          (try (let [datum-map {"noDefault" 1}]
+                 (->> (assoc datum-map "org.apache.avro.Schema" schema)
                       (.serialize (.serializer actual-serde) topic)))
                (throw (ex-message "test failed"))
                (catch SerializationException e
                  (is (= "Error serializing Avro message" (.getMessage e))))))
         (testing "forbidden nil value"
-          (try (let [m {"noDefault" 1}]
-                 (->> (assoc m "org.apache.avro.Schema" schema)
+          (try (let [datum-map {"noDefault" 1}]
+                 (->> (assoc datum-map "org.apache.avro.Schema" schema)
                       (.serialize (.serializer actual-serde) topic)))
                (throw (ex-message "test failed"))
                (catch SerializationException e
                  (is (= "Error serializing Avro message" (.getMessage e))))))))
     (testing "default field"
       (let [schema (-> (SchemaBuilder/builder)
-                       (.record "Simple")
+                       (.record "Record")
                        ^SchemaBuilder$RecordBuilder (.namespace "com.slava.core")
                        ^SchemaBuilder$FieldAssembler .fields
                        (.name "stringDefault") .type .stringType (.stringDefault "default value")
                        .endRecord)]
         (.register client topic schema)
         (testing "missing field"
-          (let [m {}
-                actual (->> (assoc m "org.apache.avro.Schema" schema)
-                            (.serialize (.serializer actual-serde) topic)
-                            (.deserialize (.deserializer control-serde) topic))
-                control (->> (.build (GenericRecordBuilder. ^Schema schema))
+          (let [datum-map {}
+                datum-record (.build (GenericRecordBuilder. ^Schema schema))
+                actual-ser (->> (assoc datum-map "org.apache.avro.Schema" schema)
+                                (.serialize (.serializer actual-serde) topic)
+                                (.deserialize (.deserializer control-serde) topic))
+                actual-de (->> datum-record
+                               (.serialize (.serializer control-serde) topic)
+                               (.deserialize (.deserializer actual-serde) topic))
+                control (->> datum-record
                              (.serialize (.serializer control-serde) topic)
                              (.deserialize (.deserializer control-serde) topic))]
-            (is (= control actual))))
+            (is (= control actual-ser))
+            (is (= (generic-data-record->map actual-ser) actual-de))))
         (testing "field present"
-          (let [m {"stringDefault" "some value"}
-                actual (->> (assoc m "org.apache.avro.Schema" schema)
-                            (.serialize (.serializer actual-serde) topic)
-                            (.deserialize (.deserializer control-serde) topic))
-                control (->> (-> (GenericRecordBuilder. ^Schema schema) (.set "stringDefault" "some value") .build)
+          (let [datum-map {"stringDefault" "some value"}
+                datum-record (-> (GenericRecordBuilder. ^Schema schema) (.set "stringDefault" "some value") .build)
+                actual-ser (->> (assoc datum-map "org.apache.avro.Schema" schema)
+                                (.serialize (.serializer actual-serde) topic)
+                                (.deserialize (.deserializer control-serde) topic))
+                actual-de (->> datum-record
+                               (.serialize (.serializer control-serde) topic)
+                               (.deserialize (.deserializer actual-serde) topic))
+                control (->> datum-record
                              (.serialize (.serializer control-serde) topic)
                              (.deserialize (.deserializer control-serde) topic))]
-            (is (= control actual))))
+            (is (= control actual-ser))
+            (is (= (generic-data-record->map actual-ser) actual-de))))
         (testing "bad type"
-          (try (let [m {"stringDefault" 1}]
-                 (->> (assoc m "org.apache.avro.Schema" schema)
+          (try (let [datum-map {"stringDefault" 1}]
+                 (->> (assoc datum-map "org.apache.avro.Schema" schema)
                       (.serialize (.serializer actual-serde) topic)))
                (throw (ex-message "test failed"))
                (catch SerializationException e
                  (is (= "Error serializing Avro message" (.getMessage e))))))
         (testing "forbidden nil value"
-          (try (let [m {"stringDefault" 1}]
-                 (->> (assoc m "org.apache.avro.Schema" schema)
+          (try (let [datum-map {"stringDefault" 1}]
+                 (->> (assoc datum-map "org.apache.avro.Schema" schema)
                       (.serialize (.serializer actual-serde) topic)))
                (throw (ex-message "test failed"))
                (catch SerializationException e
                  (is (= "Error serializing Avro message" (.getMessage e))))))))
     (testing "nullable, no-default field"
       (let [schema (-> (SchemaBuilder/builder)
-                       (.record "Simple")
+                       (.record "Record")
                        ^SchemaBuilder$RecordBuilder (.namespace "com.slava.core")
                        ^SchemaBuilder$FieldAssembler .fields
                        (.name "stringNullableNoDefault") .type .nullable .stringType .noDefault
                        .endRecord)]
         (.register client topic schema)
         (testing "missing field"
-          (try (let [m {}]
-                 (->> (assoc m "org.apache.avro.Schema" schema)
+          (try (let [datum-map {}]
+                 (->> (assoc datum-map "org.apache.avro.Schema" schema)
                       (.serialize (.serializer actual-serde) topic)))
                (throw (ex-message "test failed"))
                (catch AvroMissingFieldException e
                  (is (= "Field stringNullableNoDefault type:UNION pos:0 not set and has no default value" (.getMessage e))))))
         (testing "field present"
-          (let [m {"stringNullableNoDefault" "some value"}
-                actual (->> (assoc m "org.apache.avro.Schema" schema)
-                            (.serialize (.serializer actual-serde) topic)
-                            (.deserialize (.deserializer control-serde) topic))
-                control (->> (-> (GenericRecordBuilder. ^Schema schema) (.set "stringNullableNoDefault" "some value") .build)
+          (let [datum-map {"stringNullableNoDefault" "some value"}
+                datum-record (-> (GenericRecordBuilder. ^Schema schema) (.set "stringNullableNoDefault" "some value") .build)
+                actual-ser (->> (assoc datum-map "org.apache.avro.Schema" schema)
+                                (.serialize (.serializer actual-serde) topic)
+                                (.deserialize (.deserializer control-serde) topic))
+                actual-de (->> datum-record
+                               (.serialize (.serializer control-serde) topic)
+                               (.deserialize (.deserializer actual-serde) topic))
+                control (->> datum-record
                              (.serialize (.serializer control-serde) topic)
                              (.deserialize (.deserializer control-serde) topic))]
-            (is (= control actual)))
-          (let [m {"stringNullableNoDefault" nil}
-                actual (->> (assoc m "org.apache.avro.Schema" schema)
-                            (.serialize (.serializer actual-serde) topic)
-                            (.deserialize (.deserializer control-serde) topic))
-                control (->> (-> (GenericRecordBuilder. ^Schema schema) (.set "stringNullableNoDefault" nil) .build)
+            (is (= control actual-ser))
+            (is (= (generic-data-record->map actual-ser) actual-de)))
+          (let [datum-map {"stringNullableNoDefault" nil}
+                datum-record (-> (GenericRecordBuilder. ^Schema schema) (.set "stringNullableNoDefault" nil) .build)
+                actual-ser (->> (assoc datum-map "org.apache.avro.Schema" schema)
+                                (.serialize (.serializer actual-serde) topic)
+                                (.deserialize (.deserializer control-serde) topic))
+                actual-de (->> datum-record
+                               (.serialize (.serializer control-serde) topic)
+                               (.deserialize (.deserializer actual-serde) topic))
+                control (->> datum-record
                              (.serialize (.serializer control-serde) topic)
                              (.deserialize (.deserializer control-serde) topic))]
-            (is (= control actual))))
+            (is (= control actual-ser))
+            (is (= (generic-data-record->map actual-ser) actual-de))))
         (testing "bad type"
-          (try (let [m {"stringNullableNoDefault" 1}]
-                 (->> (assoc m "org.apache.avro.Schema" schema)
+          (try (let [datum-map {"stringNullableNoDefault" 1}]
+                 (->> (assoc datum-map "org.apache.avro.Schema" schema)
                       (.serialize (.serializer actual-serde) topic)))
                (throw (ex-message "test failed"))
                (catch SerializationException e
-                 (is (= "Error serializing Avro message" (.getMessage e))))))))))
+                 (is (= "Error serializing Avro message" (.getMessage e))))))))
+    (testing "UTF-8 magic"
+      (let [utf-8-magic "_çœ_ɵθɤɣʃʄ_ˈʕ_cA_sعربيe_æAe_胡雨軒_Петр"
+            schema (-> (SchemaBuilder/builder)
+                       (.record utf-8-magic)
+                       ^SchemaBuilder$RecordBuilder (.namespace "com.slava.core")
+                       ^SchemaBuilder$FieldAssembler .fields
+                       (.name utf-8-magic) .type .stringType .noDefault
+                       .endRecord)]
+        (let [datum-map (assoc {} utf-8-magic utf-8-magic)
+              datum-record (-> (GenericRecordBuilder. ^Schema schema) (.set utf-8-magic utf-8-magic) .build)
+              actual-ser (->> (assoc datum-map "org.apache.avro.Schema" schema)
+                              (.serialize (.serializer actual-serde) topic)
+                              (.deserialize (.deserializer control-serde) topic))
+              actual-de (->> datum-record
+                             (.serialize (.serializer control-serde) topic)
+                             (.deserialize (.deserializer actual-serde) topic))
+              control (->> datum-record
+                           (.serialize (.serializer control-serde) topic)
+                           (.deserialize (.deserializer control-serde) topic))]
+          (is (= control actual-ser))
+          (is (= (generic-data-record->map actual-ser) actual-de)))))))
