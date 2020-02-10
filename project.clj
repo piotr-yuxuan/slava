4a8362e756cb5bcc27c0fbf5c2c4c4296e197705
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2020-02-10 15:29:56 +0000

    Bump to version 0.0.19

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject slava "0.0.16"
+(defproject slava "0.0.20"
   :description "Infer specs from any Avro type"
-  :url "https://cljdoc.org/d/com.slava/com.slava/0.0.16"
+  :url "https://cljdoc.org/d/com.slava/com.slava/0.0.20"
   :license {:name "GNU GPL v3+"
             :url "http://www.gnu.org/licenses/gpl-3.0.en.html"
             :addendum "GPL_ADDITION.md"}
