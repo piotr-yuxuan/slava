0bdc79327403afe84375ddc65522684a75ba838a
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-01-28 10:13:36 +0000

    Bump to version 0.0.31

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject slava "0.0.30"
+(defproject slava "0.0.31"
   :description "Infer specs from any Avro type"
   :url "https://cljdoc.org/d/com.slava/com.slava/0.0.24"
   :license {:name "GNU GPL v3+"
