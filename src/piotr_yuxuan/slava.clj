ff6fd53004bcaee8ad59db7d5814d3e44ffb8672
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2021-04-10 20:12:43 +0100

    WIP
    https://github.com/piotr-yuxuan/slava/issues/2

--- src/piotr_yuxuan/slava.clj
+++ src/piotr_yuxuan/slava.clj
@@ -1,51 +1,61 @@
 (ns piotr-yuxuan.slava
   "FIXME add cljdoc"
-  (:require [piotr-yuxuan.slava.encode :refer [encode]]
+  (:require [piotr-yuxuan.slava.config :as config]
             [piotr-yuxuan.slava.decode :refer [decode]]
-            [piotr-yuxuan.slava.config :as config])
-  (:import (io.confluent.kafka.schemaregistry.avro AvroSchema)
-           (io.confluent.kafka.schemaregistry.client SchemaRegistryClient)
+            [piotr-yuxuan.slava.encode :refer [encode]])
+  (:import (clojure.lang Atom)
+           (io.confluent.kafka.schemaregistry.avro AvroSchema)
+           (io.confluent.kafka.schemaregistry.client SchemaRegistryClient CachedSchemaRegistryClient)
            (io.confluent.kafka.serializers KafkaAvroSerializer KafkaAvroSerializerConfig KafkaAvroDeserializer)
            (io.confluent.kafka.serializers.subject.strategy SubjectNameStrategy)
+           (java.nio ByteBuffer)
            (java.util Map)
            (org.apache.avro Schema)
-           (org.apache.kafka.common.serialization Serializer Deserializer Serdes Serde)
-           (clojure.lang Atom)
-           (org.apache.avro.generic GenericContainer)))
+           (org.apache.avro.generic GenericContainer)
+           (org.apache.kafka.common.serialization Serializer Deserializer Serdes Serde)))
 
-(defn subject-name
+(defn ^String subject-name
   "FIXME add cljdoc"
   [{:keys [key? ^SubjectNameStrategy subject-name-strategy]} ^String topic]
   (.subjectName subject-name-strategy topic key? nil))
 
-(defn resolve-subject-name
+(defn ^String resolve-subject-name
   "FIXME add cljdoc"
   [config ^String topic m]
-  (if (contains? (meta m) :piotr-yuxuan.slava/subject-name)
-    (get (meta m) :piotr-yuxuan.slava/subject-name)
+  (if (contains? (meta m) ::subject-name)
+    (get (meta m) ::subject-name)
     (subject-name config topic)))
 
-(defn schema-id!
+(defn cached-schema!
   "FIXME add cljdoc"
-  ;; FIXME on every serialization. Should be cached.
-  [^SchemaRegistryClient inner-client ^String subject-name]
-  (.getId (.getLatestSchemaMetadata inner-client subject-name)))
+  [^CachedSchemaRegistryClient inner-client schema-id]
+  (.rawSchema ^AvroSchema (.getSchemaById inner-client schema-id)))
 
-(defn resolve-schema-id
+(defn ^Schema resolve-schema
   "FIXME add cljdoc"
-  [inner-client ^Map m ^String subject-name]
-  (if (contains? (meta m) :piotr-yuxuan.slava/schema-id)
-    (get (meta m) :piotr-yuxuan.slava/schema-id)
-    (schema-id! inner-client subject-name)))
+  [config ^CachedSchemaRegistryClient inner-client topic ^Map m]
+  (cond (contains? (meta m) ::schema) (get (meta m) ::schema) ; User-defined, takes precedence.
+        (contains? (meta m) ::writer-schema) (get (meta m) ::writer-schema)
+        (contains? (meta m) ::reader-schema) (get (meta m) ::reader-schema)
+        (contains? (meta m) ::schema-id) (cached-schema! inner-client (get (meta m) ::schema-id))
+        :else (->> (resolve-subject-name config topic m)
+                   ((get-in config [:subject-name->id :through]))
+                   (cached-schema! inner-client))))
 
-(defn resolve-schema
-  "FIXME add cljdoc"
-  ^Schema [^SchemaRegistryClient inner-client ^Map m schema-id]
-  (cond (contains? (meta m) :piotr-yuxuan.slava/writer-schema) (get (meta m) :piotr-yuxuan.slava/writer-schema)
-        (contains? (meta m) :piotr-yuxuan.slava/reader-schema) (get (meta m) :piotr-yuxuan.slava/reader-schema)
-        :else (.rawSchema ^AvroSchema (.getSchemaById inner-client schema-id))))
+(defn subject-name->id
+  [inner-client value]
+  (let [found (get value :subject-name->id)]
+    (if (= :default found)
+      (let [subject-name->id (atom {})]
+        {:ref subject-name->id
+         :through (fn stub-through [subject-name]
+                    (get @subject-name->id subject-name
+                         (let [retrieved-id (.getId (.getLatestSchemaMetadata inner-client subject-name))]
+                           (swap! subject-name->id assoc subject-name retrieved-id)
+                           retrieved-id)))})
+      found)))
 
-(defn subject-name-strategy
+(defn ^SubjectNameStrategy subject-name-strategy
   "FIXME add cljdoc"
   [inner-config key?]
   (let [inner-config-obj (KafkaAvroSerializerConfig. inner-config)]
@@ -55,11 +65,12 @@
 
 (defn configure!
   "FIXME add cljdoc"
-  [{:keys [config inner]} value key?]
+  [{:keys [config inner inner-client]} value key?]
   (let [inner-config (->> value
                           (remove (comp config/slava-key? key))
                           (into {}))]
     (reset! config (assoc value
+                     :subject-name->id (subject-name->id inner-client value)
                      :key? key?
                      :subject-name-strategy (subject-name-strategy inner-config key?)))
     ;; Reflection warning: either a KafkaAvroSerializer or a KafkaAvroDeserializer.
@@ -71,12 +82,19 @@
   Serializer
   (configure [this value key?] (configure! this value key?))
   (serialize [_ topic m]
-    (->> (resolve-subject-name @config topic m)
-         (resolve-schema-id inner-client m)
-         (resolve-schema inner-client m)
+    (->> (resolve-schema config inner-client topic m)
          (encode @config m)
          (.serialize inner topic)))
   (close [_] (.close inner)))
+
+(def int-size
+  "In the JVM, an int always uses 4 bytes."
+  4)
+
+(defn schema-id
+  "Extract the schema id as known in the schema registry."
+  [data]
+  (.getInt (ByteBuffer/wrap data 0 int-size)))
 
 (defrecord ClojureDeserializer [^Atom config
                                 ^KafkaAvroDeserializer inner
@@ -86,15 +104,15 @@
   (deserialize [_ topic data]
     (let [^GenericContainer generic-container (.deserialize inner topic data)
           reader-schema (.getSchema generic-container)
-          m (decode @config generic-container reader-schema)
-          subject-name (resolve-subject-name @config topic m)]
+          m (decode @config generic-container reader-schema)]
       (vary-meta m assoc
-                 :piotr-yuxuan.slava/reader-schema reader-schema
-                 :piotr-yuxuan.slava/subject-name subject-name
-                 :piotr-yuxuan.slava/schema-id (resolve-schema-id inner-client m subject-name))))
+                 ::reader-schema reader-schema
+                 ::subject-name (subject-name @config topic)
+                 ::schema-id (schema-id data))))
   (close [_] (.close inner)))
 
 (defn ^ClojureSerializer serializer
+  "FIXME add cljdoc"
   ([inner-client]
    (ClojureSerializer. (atom nil) (KafkaAvroSerializer. inner-client) inner-client))
   ([inner-client config key?]
@@ -102,6 +120,7 @@
      (.configure config key?))))
 
 (defn ^ClojureDeserializer deserializer
+  "FIXME add cljdoc"
   ([inner-client]
    (ClojureDeserializer. (atom nil) (KafkaAvroDeserializer. inner-client) inner-client))
   ([inner-client config key?]
@@ -109,6 +128,7 @@
      (.configure config key?))))
 
 (defn ^Serde clojure-serde
+  "FIXME add cljdoc"
   ([inner-client]
    (Serdes/serdeFrom (serializer inner-client)
                      (deserializer inner-client)))
