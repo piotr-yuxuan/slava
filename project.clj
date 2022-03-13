b61b7db2548af97153bc612150876da0e7bc3368
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2022-03-13 17:27:29 +0000

    Update versions, use provided.

--- project.clj
+++ project.clj
@@ -12,18 +12,18 @@
   :dependencies [[byte-streams/byte-streams "0.2.5-alpha2"]
                  [camel-snake-kebab/camel-snake-kebab "0.4.2"]
                  [com.github.piotr-yuxuan/slava-record "0.0.1"]
-                 [io.confluent/kafka-avro-serializer "7.0.1"] ; FIXME provided
-                 [org.apache.avro/avro "1.11.0"] ; FIXME provided
                  [potemkin/potemkin "0.4.5"]]
   :aot :all
   :profiles {:github {:github/topics ["clojure" "kafka" "avro" "schema-registry"
                                       "serdes" "serde" "confluent" "kafka-streams"
                                       "avro-kafka" "avro-schema-registry"]}
-             :provided {:dependencies [[org.clojure/clojure "1.10.3"]]}
+             :provided {:dependencies [[org.clojure/clojure "1.11.0-rc1"]
+                                       [io.confluent/kafka-avro-serializer "7.0.1"]
+                                       [org.apache.avro/avro "1.11.0"]]}
              :dev {:global-vars {*warn-on-reflection* true}}
              :test {:dependencies [[com.bakdata.fluent-kafka-streams-tests/schema-registry-mock "2.5.3"]
-                                   [org.apache.kafka/kafka-clients "6.1.1-ce"]
-                                   [org.apache.kafka/kafka-streams-test-utils "6.1.1-ce"]]}
+                                   [org.apache.kafka/kafka-clients "7.0.1-ce"]
+                                   [org.apache.kafka/kafka-streams-test-utils "7.0.1-ce"]]}
              :jar {:jvm-opts ["-Dclojure.compiler.disable-locals-clearing=false"
                               "-Dclojure.compiler.direct-linking=true"]}
              :kaocha [:test {:dependencies [[lambdaisland/kaocha "1.63.998"]]}]}
