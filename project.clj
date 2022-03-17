f52a7ea9f69a06787a91ca94e814d9fe410286a5
Walter CI <piotr-yuxuan@users.noreply.github.com>
2022-03-17 18:40:06 +0000

    Update versions

--- project.clj
+++ project.clj
@@ -26,7 +26,7 @@
                                    [org.apache.kafka/kafka-streams-test-utils "7.0.1-ce"]]}
              :jar {:jvm-opts ["-Dclojure.compiler.disable-locals-clearing=false"
                               "-Dclojure.compiler.direct-linking=true"]}
-             :kaocha [:test {:dependencies [[lambdaisland/kaocha "1.63.998"]]}]}
+             :kaocha [:test {:dependencies [[lambdaisland/kaocha "1.64.1010"]]}]}
   :repositories [["confluent" {:url "https://packages.confluent.io/maven/"}]]
   :deploy-repositories [["clojars" {:sign-releases false
                                     :url "https://clojars.org/repo"
