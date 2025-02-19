e638143e1898bc0d3122911fba2973a11b25c6cc
Walter CI <piotr-yuxuan@users.noreply.github.com>
2025-02-19 18:41:52 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -19,12 +19,12 @@
                                       "avro-kafka" "avro-schema-registry"]
                       :github/private? false}
              :provided {:dependencies [[org.clojure/clojure "1.12.0"]
-                                       [io.confluent/kafka-avro-serializer "7.8.1"]
+                                       [io.confluent/kafka-avro-serializer "7.9.0"]
                                        [org.apache.avro/avro "1.12.0"]]}
              :dev {:global-vars {*warn-on-reflection* true}}
              :test {:dependencies [[com.bakdata.fluent-kafka-streams-tests/schema-registry-mock "2.16.0"]
-                                   [org.apache.kafka/kafka-clients "7.8.1-ce"]
-                                   [org.apache.kafka/kafka-streams-test-utils "7.8.1-ce"]]}
+                                   [org.apache.kafka/kafka-clients "7.9.0-ce"]
+                                   [org.apache.kafka/kafka-streams-test-utils "7.9.0-ce"]]}
              :jar {:jvm-opts ["-Dclojure.compiler.disable-locals-clearing=false"
                               "-Dclojure.compiler.direct-linking=true"]}
              :kaocha [:test {:dependencies [[lambdaisland/kaocha "1.91.1392"]]}]}
