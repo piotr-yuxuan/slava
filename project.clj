979bd7919f850894a581f6407ad4c139bd517514
Walter CI <piotr-yuxuan@users.noreply.github.com>
2025-08-26 06:48:21 +0000

    Update dependency versions

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
                                       "serdes" "serde" "confluent" "kafka-streams"
                                       "avro-kafka" "avro-schema-registry"]
                       :github/private? false}
-             :provided {:dependencies [[org.clojure/clojure "1.12.1"]
+             :provided {:dependencies [[org.clojure/clojure "1.12.2"]
                                        [io.confluent/kafka-avro-serializer "8.0.0"]
                                        [org.apache.avro/avro "1.12.0"]]}
              :dev {:global-vars {*warn-on-reflection* true}}
